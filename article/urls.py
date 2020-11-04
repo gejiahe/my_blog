5ed2e421e2da40db05bd8592ec40f4510bdc2d4b
zzgzjun <2589641825@qq.com>
2020-11-04 21:48:15 +0800

    初步完成文章列表，文章详情（包括代码高亮），前台写文章

--- article/urls.py
+++ article/urls.py
@@ -5,5 +5,7 @@ app_name='article'
 
 
 urlpatterns = [
-    path('article_list/',views.article_list,name='article_list')
+    path('article_list/',views.article_list,name='article_list'),
+    path('article_detail/<int:id>/',views.article_detail,name='article_detail'),
+    path('article_create/',views.article_create,name='article_create'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
+from .models import ArticlePost
+import markdown
 
 # Create your views here.
 def article_list(request):
-    return HttpResponse("helloword")
+    articles=ArticlePost.objects.all()
+
+    return render(request,'article/list.html',{'articles':articles})
+
+
+def article_detail(request,id):
+    article=ArticlePost.objects.get(id=id)
+    article.body=markdown.markdown(article.body,
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+    return render(request, 'article/detail.html', {'article': article})
+
+
+def article_create(request):
+    if request.method=="POST":
+        article_post_form=ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article=article_post_form.save(commit=False)
+            new_article.author=User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form=ArticlePostForm()
+        context={"article_post_form":article_post_form}
+        return render(request,'article/create.html',context)
